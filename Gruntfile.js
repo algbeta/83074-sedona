47f2317c3c31532ee3842b210f22f7c35f4a82d8
natallia <gorbunovano@gmail.com>
2016-02-26 12:52:29 +0300

    adding final files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,63 @@
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-githooks');
-  grunt.loadNpmTasks('grunt-lintspaces');
-  grunt.loadNpmTasks('grunt-spritesmith');
+  require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    htmlmin:{
+      options:{
+          removeComments: true,
+          keepClosingSlash: false
+      },
+      html:{
+          files:{
+             "index.min.html": "index.html",
+             "post.min.html": "post.html",
+             "blog.min.html": "blog.html",
+             "form.min.html":"form.html"
+          }
+      }
+    },
+
+    imagemin:{
+      images:{
+          options: {
+              optimizationLevel: 3
+          },
+          files:[{
+              expand: true,
+              src:["build/img/**/*.{png}", "build/img/**/*.{jpg}","build/img/**/*.{jpeg}"]
+          }]
+      }
+    },
+
+    cmq:{
+       style:{
+           files:{
+               "build/css/style.css" : ["build/css/style.css"]
+           }
+       }
+    },
+
+    csscomb: {
+        style: {
+           /* options: {
+               config: 'sort.json'
+            },*/
+            expand: true,
+            src: ["src/sass/**/*.scss"]
+          }
+      },
+
+    autoprefixer: {
+        options: {
+            browsers: ['last 2 versions']
+        },
+        style: {
+           src: "build/css/style.css"
+        }
+    },
 
     less: {
       style: {
@@ -22,7 +70,7 @@ module.exports = function(grunt) {
     sass: {
       style: {
         files: {
-          'src/css/style.css': 'sass/style.scss'
+          'build/css/style.css': 'sass/style.scss'
         }
       }
     },
@@ -34,7 +82,7 @@ module.exports = function(grunt) {
        },
        target: {
             files: {
-                'css/style.css': ['src/css/style.css']
+                'build/css/style.css': ['build/css/style.css']
             }
        }
     },
@@ -42,41 +90,17 @@ module.exports = function(grunt) {
     uglify: {
         target: {
             files: {
-                'js/app.min.js': ['src/js/application.js']
+                'build/js/app.min.js': ['build/js/application.js']
             }
         }
     },
-
-    watch: {
-       css: {
-            files: 'sass/*.scss',
-            tasks: ['sass','cssmin']
-        },
-        js: {
-            files: 'src/js/appliction.js',
-            tasks: ['uglify']
-        }
-    },
-
-    //generate sprite
-    sprite:{
-       icons: {
-           src: 'src/img/*.png',
-           dest:'img/icons.png',
-           destCss: 'sass/sprites/_icons-sprite.scss',
-           cssTemplate: 'src/template.scss.handlebars',
-           padding: 2,
-           cssSpritesheetName: 'icon'
-       }
-    },
-
     lintspaces: {
       test: {
         src: [
           '*.html',
           'js/*.js',
           'less/*.less',
-          'sass/*.sass'
+          'sass/*.sass',
         ],
         options: {
           editorconfig: '.editorconfig'
@@ -102,6 +126,31 @@ module.exports = function(grunt) {
           ],
           dest: 'gosha',
         }]
+      },
+      build: {
+          files: [{
+              expand: true,
+
+              src:[
+                  "img/**",
+                  "js/**",
+                  "*.html"
+              ],
+              dest:"build"
+          },
+          {
+              expand:true,
+              cwd: "bower_components",
+              src:[
+                  "picturefill/dist/picturefill.min.js",
+                  "svg-injector/dist/svg-injector.min.js",
+                  "tap/dist/tap.min.js"
+              ],
+              dest:"build/js/libs/",
+              rename:function(dest,src){
+                  return dest+(src.substring(src.lastIndexOf('/')+1, src.length));
+              }}
+          ]
       }
     },
 
@@ -109,16 +158,32 @@ module.exports = function(grunt) {
       gosha: [
         'gosha/img/README',
         'gosha/js/README'
-      ]
+      ],
+      build: ["build"],
+      afterBuild : ["build/js/application.js"]
     }
   });
 
   grunt.registerTask('test', ['lintspaces:test']);
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default',['watch']);
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('build',
+      [
+        'clean:build',
+        'copy:build',
+        'imagemin',
+        'csscomb',
+        'sass',
+        'cmq',
+        'autoprefixer',
+        'cssmin',
+        'uglify',
+        'clean:afterBuild'
+      ]
 
+  );
+  
+  grunt.registerTask('default', ['build']);
+  
   if (grunt.file.exists(__dirname, 'less', 'style.less')) {
     grunt.registerTask('gosha', ['less:style', 'copy:gosha', 'clean:gosha']);
   } else if (grunt.file.exists(__dirname, 'sass', 'style.scss')) {
---
+++ js/application.js
@@ -0,0 +1,146 @@
+function showMap(mapObjects, mapIconUrl){
+    var centerlat = .0, centerlng = .0;
+
+    for (var i = 0; i < mapObjects.length; i++) {
+        centerlat += mapObjects[i].lat;
+        centerlng += mapObjects[i].lng;
+    }
+
+   /* centerlat = centerlat / mapObjects.length + 0.5;    // +0.5 to fit map
+    centerlng = mapObjects.length;*/
+    var mapConfOptions = {
+        zoom: 6,
+        center: new google.maps.LatLng(centerlat, centerlng),
+        zoomControl: true,
+        mapTypeControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"),
+        mapConfOptions);
+
+    for (i = 0; i < mapObjects.length; i++) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(mapObjects[i].lat, mapObjects[i].lng),
+            map: map,
+            icon: mapIconUrl
+        });
+    }
+};
+
+(function(){
+    //picturefill snippet
+    document.createElement( "picture" );
+
+    //svg injection
+    var svgs = document.querySelectorAll('.page-footer__copyright-author-link img, .page-footer__link img, .survey-complex-value__img');
+    SVGInjector(svgs);
+
+    //navbar menu actions
+    var toggleButtons = document.querySelectorAll(".navbar__toggle-btn, .navbar__close");
+    if(toggleButtons && toggleButtons.length > 0){
+        //no foreach for nodelist :(
+        for (var i = 0; i < toggleButtons.length; ++i){
+            toggleButtons[i].addEventListener('click', function(){
+                document.querySelector(".navbar__menu").classList.toggle('navbar__menu--expanded');
+            });
+        }
+    }
+
+    //form complex fields actions
+    var complexValues = document.querySelectorAll(".survey-complex-value__decrease, .survey-complex-value__increase");
+    if(complexValues && complexValues.length > 0){
+        for(var i = 0; i < complexValues.length; ++i){
+            complexValues[i].addEventListener('click', function(){
+                //we always have only one input in complex value but not sure if min/max are always set so adding additional check
+                var input = this.parentElement.querySelector('input');
+                if(this.classList.contains('survey-complex-value__decrease') &&
+                    ( input.attributes.min ? input.value >input.attributes.min.value : input.value > 0)){
+                        input.value= Number(input.value) - 1;
+                        if(this.classList.contains('survey-complex-value__decrease--travel'))removeTraveler();
+                }else if(this.classList.contains('survey-complex-value__increase') &&
+                        (input.attributes.max ? input.value < input.attributes.max.value : true)){
+                        input.value = Number(input.value) +  1;
+                        if(this.classList.contains('survey-complex-value__increase--travel'))addTraveler(template.innerHTML);
+                }
+            });
+        }
+    }
+    initFormPreview();
+
+}());
+
+function addTraveler(html){
+    var li = document.createElement('li')
+    li.classList.add('survey-travelers__item');
+    li.innerHTML = html;
+    document.querySelector("#travelers").appendChild(li);
+};
+
+function removeTraveler(){
+    document.querySelector("#travelers").removeChild(
+        document.querySelector('#travelers li:last-child'));
+}
+
+function initPopups(){
+    var submitBtn = document.querySelector(".survey__field-value-submit");
+
+    submitBtn.addEventListener('click', function(e){
+        document.querySelector('.popup--failure').classList.add('popup--visible');
+    });
+
+    document.querySelector('.popup--failure .popup__btn').addEventListener('click',function(e){
+        document.querySelector('.popup--failure').classList.remove('popup--visible');
+        document.querySelector('.popup--success').classList.add('popup--visible');
+    });
+
+    document.querySelector('.popup--success .popup__btn').addEventListener('click', function(e){
+        document.querySelector('.popup--success').classList.remove('popup--visible');
+    });
+};
+
+function initFormPreview(){
+    if (!("FormData" in window) || !("FileReader" in window)) {
+        return;
+    }
+
+    var form = document.querySelector("#survey");
+    if(form) {
+        form.querySelector("#fileInput").addEventListener("change", function () {
+            var files = this.files;
+            for (var i = 0; i < files.length; i++) {
+                preview(files[i]);
+            }
+        });
+    }
+
+};
+
+function preview(file){
+    if(file.type.match(/image.*/)){
+        var reader = new FileReader();
+        reader.addEventListener('load', function(event){
+
+            var li = document.createElement('li');
+            li.classList.add('survey-gallery__item');
+            li.innerHTML = document.querySelector('#imagePreview').innerHTML;
+
+            var img = document.createElement("img");
+            img.src = event.target.result;
+            img.alt = file.name;
+            img.classList.add('survey-galley__item-image');
+
+            li.appendChild(img);
+
+            li.querySelector('.survey-gallery__item-close').addEventListener('click', function(e){
+                document.querySelector(".survey-gallery").removeChild(e.target.parentNode);
+            });
+
+            li.querySelector('.survey-gallery__item-name').innerHTML = file.name;
+
+            document.querySelector('.survey-gallery').appendChild(li);
+
+        });
+
+        reader.readAsDataURL(file);
+    }
+}
